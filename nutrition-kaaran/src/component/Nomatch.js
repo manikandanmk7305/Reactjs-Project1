034c420946e8fcaa2d333918a969b696692a2d73
manikandanmk7305 <manikandanmk7305@gmail.com>
2024-06-19 12:26:14 +0530

    feat: re config

--- nutrition-kaaran/src/component/Nomatch.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Nomatch = () => {
-  return (
-    <div>page not found</div>
-  )
-}
--- nutrition-kaaran/src/component/Service.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-export const Service = () => {
-  return (
-    <div>
-      Services
-    </div>
-  )
-}
